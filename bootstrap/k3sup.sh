bf81b51f6db4cd5493db47a55aebcd9ffa8ff76b
Rafael Ribeiro <rafael.ntw@gmail.com>
2021-09-24 23:42:49 +0100

    initial commit

---
+++ bootstrap/k3sup.sh
@@ -0,0 +1,15 @@
+k3sup install --ip="10.0.0.2" --user="pi" --k3s-version="v1.21.4+k3s1" --k3s-extra-args="--disable servicelb --disable traefik --disable metrics-server"
+
+k3sup join \
+    --ip=10.0.0.18 \
+    --server-ip=10.0.0.2 \
+    --server-user=pi \
+    --k3s-version=v1.21.4+k3s1 \
+    --user=brr
+
+k3sup join \
+    --ip=10.0.0.27 \
+    --server-ip=10.0.0.2 \
+    --server-user=pi \
+    --k3s-version=v1.21.4+k3s1 \
+    --user=pi
