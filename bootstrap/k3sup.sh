de105c6dee7a9c7ce8d585759df9404a83618388
Rafael Ribeiro <rafael.ntw@gmail.com>
2021-09-26 02:04:51 +0100

    comment out rook-ceph for now

--- bootstrap/k3sup.sh
+++ bootstrap/k3sup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-k3sup install --ip="10.0.0.2" --user="pi" --k3s-version="v1.21.4+k3s1" --k3s-extra-args="--disable servicelb --disable traefik --disable metrics-server"
+k3sup install --ip="10.0.0.18" --user="brr" --k3s-version="v1.21.4+k3s1" --k3s-extra-args="--disable servicelb --disable traefik --disable metrics-server"
 
 k3sup join \
-    --ip=10.0.0.18 \
-    --server-ip=10.0.0.2 \
-    --server-user=pi \
+    --ip=10.0.0.2 \
+    --server-ip=10.0.0.18 \
+    --server-user=brr \
     --k3s-version=v1.21.4+k3s1 \
-    --user=brr
+    --user=pi
 
 k3sup join \
     --ip=10.0.0.27 \
-    --server-ip=10.0.0.2 \
-    --server-user=pi \
+    --server-ip=10.0.0.18 \
+    --server-user=brr \
     --k3s-version=v1.21.4+k3s1 \
     --user=pi
