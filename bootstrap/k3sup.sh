4ef75bb04b9baa50a19f5490c4b75d2371bec8db
Rafael Ribeiro <rafael.ntw@gmail.com>
2022-05-24 20:16:53 +0100

    feat: cilium ipam range

--- bootstrap/k3sup.sh
+++ bootstrap/k3sup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-K3S_VERSION=v1.23.5+k3s1
+K3S_VERSION=v1.24.0-rc1+k3s1
 K3S_SERVER_IP="10.0.1.200"
 K3S_USER="rafaribe"
 k3sup install --ip=$K3S_SERVER_IP --user=$K3S_USER --k3s-version="${K3S_VERSION}" --k3s-extra-args="--disable servicelb --disable traefik --disable local-storage --flannel-backend=none --disable-network-policy" --local-path=./kubeconfig --ssh-key=/home/rafaribe/.ssh/id_ed25519
