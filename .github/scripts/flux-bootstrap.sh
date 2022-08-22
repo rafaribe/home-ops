d11c2ced0dd7089919a3afed8859ae5f3fe724e7
rafaribe <rafael.ntw@gmail.com>
2022-08-23 00:58:25 +0100

    feat: bootstrap #2

---
+++ .github/scripts/flux-bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+flux bootstrap github \
+    --owner=rafaribe \
+    --repository=home-ops \
+    --branch=main \
+    --path=kubernetes/clusters/delta/flux \
+    --read-write-key \
+    --token-auth
--- .github/scripts/k3sup.sh
+++ .github/scripts/k3sup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-K3S_VERSION=v1.24.0-rc1+k3s1
+K3S_VERSION=v1.24.3+k3s1
 K3S_SERVER_IP="10.0.1.200"
 K3S_USER="rafaribe"
 k3sup install --ip=$K3S_SERVER_IP --user=$K3S_USER --k3s-version="${K3S_VERSION}" --k3s-extra-args="--disable servicelb --disable traefik --disable local-storage --flannel-backend=none --disable-network-policy" --local-path=./kubeconfig --ssh-key=/home/rafaribe/.ssh/id_ed25519
@@ -19,4 +19,8 @@ k3sup join \
     --server-user=$K3S_USER \
     --k3s-version="${K3S_VERSION}" \
     --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
-cp ./kubeconfig ~/.kube/teivas
+cp ./kubeconfig ~/.kube/home-ops
+cd ~/.kube && ./update-kubeconfig.sh
+kubectx default
+
+helm install cilium cilium/cilium --values values.yaml
