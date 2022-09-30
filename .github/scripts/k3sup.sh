450d6a831d92f6d1684796e85c52b87c4fc14322
Rafael <rafael.ntw@gmail.com>
2022-09-30 22:33:55 +0100

    chore: deploy again

--- .github/scripts/k3sup.sh
+++ .github/scripts/k3sup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-K3S_VERSION=v1.24.3+k3s1
+K3S_VERSION=v1.25.2+k3s1
 K3S_SERVER_IP="10.0.1.200"
 K3S_USER="rafaribe"
 k3sup install --ip=$K3S_SERVER_IP --user=$K3S_USER --k3s-version="${K3S_VERSION}" --k3s-extra-args="--disable servicelb --disable traefik --disable local-storage --flannel-backend=none --disable-network-policy" --local-path=./kubeconfig --ssh-key=/home/rafaribe/.ssh/id_ed25519
@@ -11,7 +11,6 @@ k3sup join \
     --server-user=$K3S_USER \
     --k3s-version="${K3S_VERSION}" \
     --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
-
 
 k3sup join \
     --ip=10.0.1.202 \
