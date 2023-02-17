555dcda5fe5825dba6d04b6a602c700b9ac39b07
Rafael <rafael.ntw@gmail.com>
2023-02-17 01:56:08 +0000

    fix: flux

--- .github/scripts/flux-bootstrap.sh
+++ .github/scripts/flux-bootstrap.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-flux bootstrap github \
-    --owner=rafaribe \
-    --repository=home-ops \
-    --branch=main \
-    --path=kubernetes/clusters/truenas-scale/flux \
-    --read-write-key \
-    --token-auth
-
 # flux bootstrap github \
 #     --owner=rafaribe \
 #     --repository=home-ops \
 #     --branch=main \
-#     --path=kubernetes/clusters/delta/flux \
+#     --path=kubernetes/clusters/truenas-scale/flux \
 #     --read-write-key \
 #     --token-auth
+
+flux bootstrap github \
+    --owner=rafaribe \
+    --repository=home-ops \
+    --branch=main \
+    --path=kubernetes/clusters/delta/flux \
+    --read-write-key \
+    --token-auth
