3407dc415120e4dbd6dba2273d912f3cd4d65293
Rafael <rafael.ntw@gmail.com>
2023-02-21 19:11:29 +0000

    feat: add way to create service tokens for doppler

--- terraform/doppler/outputs.tf
+++ terraform/doppler/outputs.tf
@@ -1,28 +1,14 @@
 # We need this data source for the remote terraform state file
-data "terraform_remote_state" "project_instances" {
-  backend = "remote"
 
-  config = {
-    hostname     = "app.terraform.io"
-    organization = "rafaribe"
-
-    workspaces = {
-      name = "home-doppler"
-    }
+output "all_service_tokens" {
+  value = {
+    for module_name, module_outputs in {
+      "cert-manager" = module.cert-manager.service_tokens
+      "cloudflare"   = module.cloudflare.service_tokens
+      "authentik"    = module.authentik.service_tokens
+      "unifi"        = module.unifi.service_tokens
+    } :
+    module_name => module_outputs
   }
+  sensitive = true
 }
-
-# output "doppler_service_tokens" {
-#   value = merge(flatten([
-#     for instance_state in data.terraform_remote_state.project_instances.outputs : [
-#       for environment, config in instance_state.service_tokens : {
-#         "${instance_state.project_name}-${environment}" = {
-#           project       = instance_state.project_name
-#           environment   = environment
-#           service_token = config.name
-#         }
-#       }
-#     ]
-#   ]))
-# }
-
--- terraform/doppler/project/outputs.tf
+++ terraform/doppler/project/outputs.tf
@@ -1,4 +1,10 @@
 # Access the secret value
 output "service_tokens" {
-  value = { for env, token in doppler_service_token.service_token : env => token.key  }
+  value = { for env in toset(var.environments) :
+    env => {
+      key         = doppler_service_token.service_token[env].key
+      environment = env
+    }
+  }
+  sensitive = false
 }
