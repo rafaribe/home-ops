4a43748fc2b16db92448db3eec2aadb5c5a26212
Rafael <rafael.ntw@gmail.com>
2023-02-22 02:09:24 +0000

    feat: add networks to unifi

--- terraform/unifi/networks.tf
+++ terraform/unifi/networks.tf
@@ -3,14 +3,27 @@ locals {
   trusted_vlan_id   = 10
 }
 
-resource "unifi_network" "main_network" {
-  name          = "home-network"
+resource "unifi_network" "default" {
+  name    = "home-network"
+  purpose = "corporate"
+  subnet  = "10.0.1.1/22"
+  # vlan_id      = local.trusted_vlan_id
+  dhcp_start   = "10.0.3.1"
+  dhcp_stop    = "10.0.3.254"
+  dhcp_enabled = true
+  domain_name  = "home"
+  dhcp_dns     = ["10.0.1.250", "1.1.1.1", "9.9.9.9", "8.8.8.8"]
+}
+
+resource "unifi_network" "iot" {
+  name          = "iot"
   purpose       = "corporate"
-  subnet        = "10.0.1.1/22"
-  vlan_id       = local.trusted_vlan_id
-  dhcp_start    = "10.0.3.1"
-  dhcp_stop     = "10.0.3.254"
+  subnet        = "10.0.4.0/24"
+  vlan_id       = local.untrusted_vlan_id
+  dhcp_start    = "10.0.4.2"
+  dhcp_stop     = "10.0.4.254"
   dhcp_enabled  = true
   domain_name   = "home"
-  dhcp_dns      = ["10.0.1.250", "1.1.1.1", "9.9.9.9", "8.8.8.8"]
+  dhcp_dns      = ["1.1.1.1", "9.9.9.9", "8.8.8.8"]
+  igmp_snooping = true
 }
