8e68431c7394582c56bf9bd04d32871a75d9d7d8
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-05-02 11:24:39 +0100

    fix: add k3sup stuff

--- .github/scripts/k3sup.sh
+++ .github/scripts/k3sup.sh
@@ -8,7 +8,7 @@ k3sup install \
     --user=$K3S_USER \
     --ip=$K3S_SERVER_IP \
     --k3s-version="${K3S_VERSION}" \
-    --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519 \
+    --user=$K3S_USER --ssh-key=$HOME/.ssh/id_ed25519 \
     --context homeops \
     --k3s-extra-args '--disable traefik --flannel-backend=none --disable-network-policy --disable=servicelb'
 
@@ -18,7 +18,7 @@ k3sup install \
 #     --server-user=$K3S_USER \
 #     --k3s-version="${K3S_VERSION}" \
 #     --user $K3S_USER \
-#     --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
+#     --user=$K3S_USER --ssh-key=$HOME/.ssh/id_ed25519
 
 k3sup join \
     --ip=10.0.1.14 \
@@ -26,11 +26,11 @@ k3sup join \
     --server-user=$K3S_USER \
     --k3s-version="${K3S_VERSION}" \
     --user $K3S_USER \
-    --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
+    --user=$K3S_USER --ssh-key=$HOME/.ssh/id_ed25519
 k3sup join \
     --ip=10.0.1.15 \
     --server-ip=$K3S_SERVER_IP \
     --server-user=$K3S_USER \
     --k3s-version="${K3S_VERSION}" \
     --user $K3S_USER \
-    --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
+    --user=$K3S_USER --ssh-key=$HOME/.ssh/id_ed25519
