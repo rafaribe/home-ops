0500e9256fdbb467e29b54164c26c50b3d68b05b
Rafael <rafael.ntw@gmail.com>
2023-05-30 23:49:10 +0100

    fix: k3sup script

--- .github/scripts/k3sup.sh
+++ .github/scripts/k3sup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-K3S_VERSION=v1.27.1+k3s1
-K3S_SERVER_IP="10.0.1.12"
+K3S_VERSION=v1.27.2+k3s1
+K3S_SERVER_IP="10.0.1.13"
 K3S_USER="brr"
 
 k3sup install \
@@ -10,15 +10,15 @@ k3sup install \
     --k3s-version="${K3S_VERSION}" \
     --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519 \
     --context homeops \
-    --k3s-extra-args '--disable traefik --flannel-backend=none --disable-network-policy'
+    --k3s-extra-args '--disable traefik --flannel-backend=none --disable-network-policy --disable=servicelb'
 
-k3sup join \
-    --ip=10.0.1.13 \
-    --server-ip=$K3S_SERVER_IP \
-    --server-user=$K3S_USER \
-    --k3s-version="${K3S_VERSION}" \
-    --user $K3S_USER \
-    --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
+# k3sup join \
+#     --ip=10.0.1.13 \
+#     --server-ip=$K3S_SERVER_IP \
+#     --server-user=$K3S_USER \
+#     --k3s-version="${K3S_VERSION}" \
+#     --user $K3S_USER \
+#     --user=$K3S_USER --ssh-key=/home/rafaribe/.ssh/id_ed25519
 
 k3sup join \
     --ip=10.0.1.14 \
