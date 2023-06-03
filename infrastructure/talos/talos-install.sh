823bef9ab3e97373816c105278b6e3660640cbf9
Rafael <rafael.ntw@gmail.com>
2023-06-03 17:10:32 +0100

    chore: reduce loki usage

--- infrastructure/talos/talos-install.sh
+++ infrastructure/talos/talos-install.sh
@@ -4,5 +4,6 @@ talosctl apply --insecure -n 10.0.1.12 --file clusterconfig/skovald-odin.yaml
 talosctl apply --insecure -n 10.0.1.13 --file clusterconfig/skovald-loki.yaml
 talosctl apply --insecure -n 10.0.1.14 --file clusterconfig/skovald-thor.yaml
 talosctl apply --insecure -n 10.0.1.15 --file clusterconfig/skovald-freya.yaml
+talosctl apply --insecure -n 10.0.1.16 --file clusterconfig/skovald-helya.yaml
 sleep 60
 talosctl -n 10.0.1.12 bootstrap
