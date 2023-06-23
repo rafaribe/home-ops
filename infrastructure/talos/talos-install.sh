14acbe844a3bc3ed5b0b7c756cc1b381ff45d4cb
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-06-23 21:35:36 +0100

    fix: talconfig

--- infrastructure/talos/talos-install.sh
+++ infrastructure/talos/talos-install.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 
-talosctl apply --insecure -n 10.0.1.12 --file clusterconfig/skovald-odin.yaml
-talosctl apply --insecure -n 10.0.1.13 --file clusterconfig/skovald-loki.yaml
-talosctl apply --insecure -n 10.0.1.14 --file clusterconfig/skovald-freya.yaml
-talosctl apply --insecure -n 10.0.1.15 --file clusterconfig/skovald-thor.yaml
-talosctl apply --insecure -n 10.0.1.16 --file clusterconfig/skovald-helya.yaml
+#talosctl apply --insecure -n 10.0.1.7 --file clusterconfig/skovald-odin.yaml
+talosctl apply --insecure -n 10.0.1.8 --file clusterconfig/skovald-loki.yaml
+talosctl apply --insecure -n 10.0.1.9 --file clusterconfig/skovald-thor.yaml
+talosctl apply --insecure -n 10.0.1.10 --file clusterconfig/skovald-freya.yaml
+echo "sleeping"
 sleep 60
-talosctl -n 10.0.1.12 bootstrap
+echo "done sleeping"
+talosctl -n 10.0.1.7 bootstrap
 
-# after provision
+# # after provision
 
-talosctl apply -n 10.0.1.12 --file clusterconfig/skovald-odin.yaml
-talosctl apply -n 10.0.1.13 --file clusterconfig/skovald-loki.yaml
+# talosctl apply -n 10.0.1.7 --file clusterconfig/skovald-odin.yaml
+# talosctl apply -n 10.0.1.8 --file clusterconfig/skovald-loki.yaml
 
-talosctl apply -n 10.0.1.14 --file clusterconfig/skovald-freya.yaml
-talosctl apply -n 10.0.1.15 --file clusterconfig/skovald-thor.yaml
-talosctl apply -n 10.0.1.16 --file clusterconfig/skovald-helya.yaml
-
-talosctl upgrade -n 10.0.1.12 --image ghcr.io/siderolabs/installer:v1.4.5
-talosctl upgrade -n 10.0.1.13 --image ghcr.io/siderolabs/installer:v1.4.5
-talosctl upgrade -n 10.0.1.14 --image ghcr.io/siderolabs/installer:v1.4.5
-talosctl upgrade -n 10.0.1.15 --image ghcr.io/siderolabs/installer:v1.4.5
-talosctl upgrade -n 10.0.1.16 --image ghcr.io/siderolabs/installer:v1.4.5
+# talosctl apply -n 10.0.1.9 --file clusterconfig/skovald-thor.yaml
+# talosctl apply -n 10.0.1.10 --file clusterconfig/skovald-freya.yaml
