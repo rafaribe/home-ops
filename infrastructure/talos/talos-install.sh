9526456413d98a17f7811e80eaae668c074b11fe
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-07-04 23:51:29 +0100

    feat: adjust talos stuff

--- infrastructure/talos/talos-install.sh
+++ infrastructure/talos/talos-install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-talosctl apply --insecure -n 10.0.1.7 --file clusterconfig/skovald-odin.yaml -m reboot
+talosctl apply --insecure -n 10.0.1.7 --file clusterconfig/skovald-odin.yaml
 talosctl apply --insecure -n 10.0.1.8 --file clusterconfig/skovald-loki.yaml
-talosctl apply --insecure -n 10.0.1.9 --file clusterconfig/skovald-thor.yaml -m reboot
+talosctl apply --insecure -n 10.0.1.9 --file clusterconfig/skovald-thor.yaml
 talosctl apply --insecure -n 10.0.1.10 --file clusterconfig/skovald-freya.yaml
 echo "sleeping"
-sleep 60
+sleep 120
 echo "done sleeping"
 talosctl -n 10.0.1.7 bootstrap
 
