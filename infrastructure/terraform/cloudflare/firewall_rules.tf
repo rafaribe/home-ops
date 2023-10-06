a97d7a06264abd4974de4c0ec730b344a06290e8
Rafael Ribeiro <rafael.ntw@gmail.com>
2023-10-06 16:07:37 +0100

    Revert "remove firewall for now firewall_rules.tf"
    This reverts commit aec3063339620d86d638f75553be4963c60bebd1.

--- infrastructure/terraform/cloudflare/firewall_rules.tf
+++ infrastructure/terraform/cloudflare/firewall_rules.tf
@@ -13,3 +13,16 @@ resource "cloudflare_firewall_rule" "terraform" {
 }
 
 # Block Not Portugal
+
+resource "cloudflare_filter" "notportugal" {
+  zone_id     = lookup(data.cloudflare_zones.domain_com.zones[0], "id")
+  description = "Block Requests that dont come from Portugal and France"
+  expression  = "(ip.geoip.country ne \"PT\") or (ip.geoip.country eq \"FR\")"
+}
+
+resource "cloudflare_firewall_rule" "notportugal" {
+  zone_id     = lookup(data.cloudflare_zones.domain_com.zones[0], "id")
+  description = "Block Requests that don't come from Portugal"
+  filter_id   = cloudflare_filter.notportugal.id
+  action      = "block"
+}
