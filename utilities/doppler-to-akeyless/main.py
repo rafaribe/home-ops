8a8ac29ab9ce9fe90f0cea0a5156a3da36196929
rafaribe <rafael.ntw@gmail.com>
2024-06-05 23:34:58 +0100

    fix: migrator updated

--- utilities/doppler-to-akeyless/main.py
+++ utilities/doppler-to-akeyless/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import json
 from pprint import pprint
 from dopplersdk import DopplerSDK
 
@@ -41,7 +42,7 @@ def setup_akeyless_sdk(akeyless_access_id, akeyless_access_key):
     return api
 
 def get_projects(doppler):
-    projects_response = doppler.projects.list(page=2)
+    projects_response = doppler.projects.list(per_page=200)
     return projects_response.projects
 
 def process_project_secrets(doppler, akeyless_api, project_name, config_name):
@@ -53,7 +54,7 @@ def process_project_secrets(doppler, akeyless_api, project_name, config_name):
         logger.debug(secret)
         secret_detail = doppler.secrets.get(project=project_name, config=config_name, name=secret)
         logger.debug(pprint(vars(secret_detail)))
-        secret_name_with_path = f'{project_name}/{secret}'
+        secret_name_with_path = f'{project_name}'
         try:
             describe_item_body = akeyless.DescribeItem(name=secret_name_with_path, token=token)
             api_response = akeyless_api.describe_item(describe_item_body)
@@ -65,7 +66,37 @@ def process_project_secrets(doppler, akeyless_api, project_name, config_name):
             secret_body =  akeyless.CreateSecret(name=secret_name_with_path, tags=[project_name], value=secret_detail.value['raw'], token = token)
             api_response = akeyless_api.create_secret(secret_body)
             logger.debug(f"Secret {secret_name_with_path} Created")
-        
+            
+def process_project_secrets_json(doppler, akeyless_api, project_name, config_name):
+    secrets_response = doppler.secrets.list(project=project_name, config=config_name)
+    secrets = secrets_response.secrets
+
+    logger.debug("Secret Names:")
+    secrets_json = {}
+    for secret in secrets:
+        if not secret.startswith("DOPPLER"):
+            secret_detail = doppler.secrets.get(project=project_name, config=config_name, name=secret)
+            secrets_json[secret_detail.name] = secret_detail.value['raw']
+    logger.debug(secrets_json)
+    value_json = json.dumps(secrets_json)
+    secret_body =  akeyless.CreateSecret(name=project_name, tags=[project_name], value=value_json, token = token, json=True, format='json')
+    api_response = akeyless_api.create_secret(secret_body)
+    logger.debug(f"Secret {project_name} Created")
+    
+def delete_all_akeyless_secrets(akeyless_api, items):
+    for item in items:
+        body = akeyless.DeleteItem(token=token, name=item)
+        res = akeyless_api.delete_item(body)
+        logger.debug(f'deleted {item}')
+    logger.debug('finish deleting')
+
+def list_all_items(akeyless_api):
+    list_body = akeyless.ListItems(token = token, auto_pagination = 'enabled')
+    list_items = akeyless_api.list_items(list_body)
+    item_paths = []
+    for item in list_items.items:
+        item_paths.append(item.item_name)
+    return item_paths
 
 def main():
     # Init envs
@@ -81,7 +112,7 @@ def main():
     config_name = 'prod'
     for project in projects_json:
         project_name = project['name']
-        process_project_secrets(doppler, akeyless_api, project_name, config_name)
+        process_project_secrets_json(doppler, akeyless_api, project_name, config_name)
 
 if __name__ == '__main__':
     main()
