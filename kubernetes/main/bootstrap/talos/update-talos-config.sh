79d38da48ed95a2a4e3e214fb54e72506e37a04b
rafaribe <rafael.ntw@gmail.com>
2024-06-09 15:38:50 +0100

    fix: templates

---
+++ kubernetes/main/bootstrap/talos/update-talos-config.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+echo "Applying configuration to Node 10.0.0.4 thor.cluster.home"
+talosctl apply -n 10.0.0.4 --file clusterconfig/main-thor.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Applying configuration to Node 10.0.0.5 loki.cluster.home"
+talosctl apply -n 10.0.0.5 --file clusterconfig/main-loki.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Applying configuration to Node 10.0.0.8 freya.cluster.home"
+talosctl apply -n 10.0.0.8 --file clusterconfig/main-freya.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Applying configuration to Node 10.0.0.3 odin.cluster.home"
+talosctl apply -n 10.0.0.3 --file clusterconfig/main-odin.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Applying configuration to Node 10.0.0.7 tyr.cluster.home"
+talosctl apply -n 10.0.0.7 --file clusterconfig/main-tyr.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Applying configuration to Node 10.0.0.9 mimir.cluster.home"
+talosctl apply -n 10.0.0.12 --file clusterconfig/main-mimir.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Applying configuration to Node 10.0.0.11 heimdall.cluster.home"
+talosctl apply -n 10.0.0.11 --file clusterconfig/main-heimdall.home.arpa.yaml --mode=auto
+sleep 2
+
+echo "Bootstrapping the cluster"
