359a1d0b3ecdbb176cf50092ce30b2efef45b2e6
rafaribe <rafael.ntw@gmail.com>
2024-06-23 23:54:59 +0100

    feat: add radicale

--- infrastructure/terraform/servarr/indexers.tf
+++ infrastructure/terraform/servarr/indexers.tf
@@ -1,35 +1,37 @@
-resource "prowlarr_indexer" "usenet_nzbgeek" {
-  enable          = true
-  name            = "NZBGeek"
-  implementation  = "Newznab"
-  config_contract = "NewznabSettings"
-  app_profile_id  = 1
-  protocol        = "usenet"
-  priority        = 1
-  tags            = []
 
-  fields = [
-    {
-      name : "baseUrl",
-      text_value : "https://api.nzbgeek.info"
-    },
-    {
-      name : "apiPath"
-      text_value : "/api"
-    },
-    {
-      name : "apiKey"
-      text_value : "${data.sops_file.servarr-secrets.data["nzbgeek_api_key"]}"
-    },
-    # {
-    #   name: "vipExpiration"
-    #   text_value: "2024-12-26"
-    # }
-  ]
-  lifecycle {
-    // ignore_changes = [all]
-  }
-}
+// Indexers currently not working correctly
+# resource "prowlarr_indexer" "usenet_nzbgeek" {
+#   enable          = true
+#   name            = "NZBGeek"
+#   implementation  = "Newznab"
+#   config_contract = "NewznabSettings"
+#   app_profile_id  = 1
+#   protocol        = "usenet"
+#   priority        = 1
+#   tags            = []
+
+#   fields = [
+#     {
+#       name : "baseUrl",
+#       text_value : "https://api.nzbgeek.info"
+#     },
+#     {
+#       name : "apiPath"
+#       text_value : "/api"
+#     },
+#     {
+#       name : "apiKey"
+#       text_value : "${data.sops_file.servarr-secrets.data["nzbgeek_api_key"]}"
+#     },
+#     # {
+#     #   name: "vipExpiration"
+#     #   text_value: "2024-12-26"
+#     # }
+#   ]
+#   lifecycle {
+#     // ignore_changes = [all]
+#   }
+# }
 
 # resource "prowlarr_indexer" "mam" {
 #   enable          = true
