3d0e775919214cba68aa2925791599219a529be6
rafaribe <rafael.ntw@gmail.com>
2024-07-03 23:40:21 +0100

    feat: add kubevirt namespace

--- infrastructure/terraform/proxmox/vm.tf
+++ infrastructure/terraform/proxmox/vm.tf
@@ -1,19 +1,19 @@
-module "nixos" {
-  source   = "./virtualmachines"
-  for_each = local.router_nodes
+# module "nixos" {
+#   source   = "./virtualmachines"
+#   for_each = local.router_nodes
 
-  name      = "nixos-${each.key}"
-  node_name = each.key
-  cpu       = local.router_nodes[each.key].cpu
-  sockets   = local.router_nodes[each.key].sockets
-  memory    = local.router_nodes[each.key].memory
-  disk_size = 20
+#   name      = "nixos-${each.key}"
+#   node_name = each.key
+#   cpu       = local.router_nodes[each.key].cpu
+#   sockets   = local.router_nodes[each.key].sockets
+#   memory    = local.router_nodes[each.key].memory
+#   disk_size = 20
 
-  iso_id      = proxmox_virtual_environment_file.nix_os_iso[each.key].id
-  vm_id       = 2000 + index(keys(local.router_nodes), each.key)
-  mac_address = "42:C3:B0:6B:80:B${index(keys(local.router_nodes), each.key) + 1}"
-  tags        = ["nixos", "network-tooling", "terraform"]
-  providers = {
-    proxmox = proxmox
-  }
-}
+#   iso_id      = proxmox_virtual_environment_file.nix_os_iso[each.key].id
+#   vm_id       = 2000 + index(keys(local.router_nodes), each.key)
+#   mac_address = "42:C3:B0:6B:80:B${index(keys(local.router_nodes), each.key) + 1}"
+#   tags        = ["nixos", "network-tooling", "terraform"]
+#   providers = {
+#     proxmox = proxmox
+#   }
+# }
