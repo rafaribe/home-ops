df86f2ca4e69ffe9606f736dea3c8e0d48af4320
rafaribe <rafael.ntw@gmail.com>
2024-11-18 20:58:46 +0000

    chore: optional stuff

--- kubernetes/main/bootstrap/talos/talos-upgrade-rk1.sh
+++ kubernetes/main/bootstrap/talos/talos-upgrade-rk1.sh
@@ -10,5 +10,7 @@ talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.12 --i
 talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.13 --image=$FORK:$VERSION --preserve;
 talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.14 --image=$FORK:$VERSION --preserve;
 talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.15 --image=$FORK:$VERSION --preserve;
-
+# Optional
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.9 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3 --preserve;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.10 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3 --preserve;:p
 echo "Finished Upgrading
