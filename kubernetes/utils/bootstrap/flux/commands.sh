c71e993fc7612f784afd524477f09f30feea917d
Rafael Ribeiro <rafael.ntw@gmail.com>
2024-11-20 10:51:28 +0000

    chore: remove utils cluster for now

--- kubernetes/utils/bootstrap/flux/commands.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl create namespace flux-system
-sops -d age-key.sops.yaml | kubectl apply -f -
-sops -d github-deploy-key.sops.yaml | kubectl apply -f -
-kubectl apply -k .
