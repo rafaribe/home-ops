dbbefe73daddb00055752c3f3de59011b6d2120e
rafaribe <rafael.ntw@gmail.com>
2024-12-04 03:27:27 +0000

    feat: move control planes to workers and workers to controlplanes

--- kubernetes/main/bootstrap/talos/talos-upgrade-rk1.sh
+++ kubernetes/main/bootstrap/talos/talos-upgrade-rk1.sh
@@ -6,11 +6,15 @@ VERSION=v1.8.3-rk3588
 
 echo "Flashing $FORK:VERSION of talos linux into the Turing Pi Rk1 Cluster"
 
-talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.12 --image=$FORK:$VERSION --preserve;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.12 --image=$FORK:$VERSION
 talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.13 --image=$FORK:$VERSION --preserve;
-talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.14 --image=$FORK:$VERSION --preserve;
-talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.15 --image=$FORK:$VERSION --preserve;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.14 --image=$FORK:$VERSION
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.15 --image=$FORK:$VERSION
 # Optional
-talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.9 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3 --preserve;
-talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.10 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3 --preserve;:p
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.3 --image=factory.talos.dev/installer/a6d0f18af362ff678bdeba1192090f74c02201e14dc5080e5f2427e272fd4b0e:v1.8.3;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.4 --image=factory.talos.dev/installer/a6d0f18af362ff678bdeba1192090f74c02201e14dc5080e5f2427e272fd4b0e:v1.8.3;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.5 --image=factory.talos.dev/installer/a6d0f18af362ff678bdeba1192090f74c02201e14dc5080e5f2427e272fd4b0e:v1.8.3;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.8 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.9 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3;
+talosctl upgrade --talosconfig=./clusterconfig/talosconfig --nodes=10.0.1.10 --image=factory.talos.dev/installer/5e314fd597ad37c510a7a6918eeab55e15ebce1b43ae338cd4b95dfaf4f791f0:v1.8.3;
 echo "Finished Upgrading
