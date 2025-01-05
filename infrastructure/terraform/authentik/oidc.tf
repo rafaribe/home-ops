7ebf80ad10c24a419b8b7932fcbc5c10e7a204e7
Rafael Ribeiro <rafael.ntw@gmail.com>
2025-01-05 15:32:36 +0000

    fix(authentik): update OIDC application names to lowercase

--- infrastructure/terraform/authentik/oidc.tf
+++ infrastructure/terraform/authentik/oidc.tf
@@ -1,17 +1,17 @@
 locals {
   oidcs = [
     {
-      authentik_oidc_application_name        = "Grafana"
+      authentik_oidc_application_name        = "grafana"
       authentik_oidc_application_icon_url    = "https://w1.pngwing.com/pngs/950/813/png-transparent-github-logo-grafana-influxdb-dashboard-visualization-web-application-installation-data-plugin-thumbnail.png"
       authentik_oidc_application_description = "Grafana OAuth application"
     },
     {
-      authentik_oidc_application_name        = "Paperless-Ngx"
+      authentik_oidc_application_name        = "paperless"
       authentik_oidc_application_icon_url    = "https://avatars.githubusercontent.com/u/99562962?s=280&v=4"
       authentik_oidc_application_description = "Paperless OAuth application"
     },
     {
-      authentik_oidc_application_name        = "PGAdmin"
+      authentik_oidc_application_name        = "pgadmin"
       authentik_oidc_application_icon_url    = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png"
       authentik_oidc_application_description = "PGAdmin OAuth application"
     },
